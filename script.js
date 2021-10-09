c983cef7715097fa60425a543bad1d64e42068c4
Amélys Krifa <doom.sebastien.pro@gmail.com>
2021-10-09 14:24:17 +0200

    scrolling script and debug done

--- script.js
+++ script.js
@@ -5,11 +5,12 @@ const answersContainers = Array.from(document.getElementsByClassName("answer_con
 for (let i = 0; i < questionsContainers.length; i++) {
     questionsContainers[i].addEventListener("click", function (event) {
 
-        if (answersContainers[i].style.height == "100px") {
+        if (answersContainers[i].style.height == answersContainers[i].scrollHeight + "px") {
             answersContainers[i].style.height = "0px"
-            questionsContainers[i].querySelector("h2").style.fontWeight = "400"
+            questionsContainers[i].querySelector("h2").style.fontWeight = ""
         } else {
-            answersContainers[i].style.height = "100px"
+            answersContainers[i].style.height = "0px"
+            answersContainers[i].style.height = answersContainers[i].scrollHeight + "px"
             questionsContainers[i].querySelector("h2").style.fontWeight = "bold"
         }
     })
